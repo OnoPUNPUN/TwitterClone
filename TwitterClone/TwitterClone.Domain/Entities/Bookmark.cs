9a76ae254b8fc5ee78dca81f6ba65489451936ff
OnoPUNPUN <punpunonodera754@gmail.com>
2026-08-08 16:18:11 +0600

    feat: Add Bookmark Class

---
+++ TwitterClone/TwitterClone.Domain/Entities/Bookmark.cs
@@ -0,0 +1,16 @@
+﻿namespace TwitterClone.Domain.Entities
+{
+    public class Bookmark
+    {
+        public Guid UserId { get; private set; }
+        public Guid TweetId { get; private set; }
+        public DateTime BookmarkedAt { get; private set; }
+
+        public Bookmark(Guid userId, Guid tweetId)
+        {
+            UserId = userId;
+            TweetId = tweetId;
+            BookmarkedAt = DateTime.UtcNow;
+        }
+    }
+}
