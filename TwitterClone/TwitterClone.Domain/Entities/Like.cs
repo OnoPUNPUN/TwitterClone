ebb191c0254a1f9c794d85ff353ae8eb3e12068a
OnoPUNPUN <punpunonodera754@gmail.com>
2026-08-08 16:12:55 +0600

    feat: Add Like Class

---
+++ TwitterClone/TwitterClone.Domain/Entities/Like.cs
@@ -0,0 +1,16 @@
+﻿namespace TwitterClone.Domain.Entities
+{
+    public class Like
+    {
+        public Guid UserId { get; private set; }
+        public Guid TweetId { get; private set; }
+        public DateTime LikedAt { get; private set; }
+
+        public Like(Guid userId, Guid tweetId)
+        {
+            UserId = userId;
+            TweetId = tweetId;
+            LikedAt = DateTime.UtcNow;
+        }
+    }
+}
