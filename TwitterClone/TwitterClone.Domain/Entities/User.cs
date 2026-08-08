f12414fe8e390d6f1dee2699cd52b9618c5d1bf3
OnoPUNPUN <punpunonodera754@gmail.com>
2026-08-08 15:36:47 +0600

    feat: Add User Entity

---
+++ TwitterClone/TwitterClone.Domain/Entities/User.cs
@@ -0,0 +1,58 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TwitterClone.Domain.Entities
+{
+    public class User
+    {
+        private Guid _id;
+        private string _username;
+        private string _email;
+        
+        public Guid Id
+        {
+            get { return _id; }
+        }
+
+        private static readonly Regex EmailRegex = new Regex(
+            @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+            RegexOptions.IgnoreCase | RegexOptions.Compiled
+        );
+
+        public string Username
+        { 
+            get { return _username; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Username cannot be null or empty.");
+                }
+                _username = value;
+            }
+        }
+
+        public string Email
+        { 
+            get { return _email; }
+            set 
+            { 
+                if(string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Email cannot be null or empty.");
+                } else if (!IsValidRegex(value))
+                {
+                    throw new ArgumentException("Email format is invalid.");
+                }
+                _email = value;
+            }
+        }
+
+        static bool IsValidRegex(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+
+            return EmailRegex.IsMatch(email);
+        }
+    }
+}
