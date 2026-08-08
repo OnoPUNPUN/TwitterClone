3873da7473a8591c3a56babce9b9cd91fc029cbb
OnoPUNPUN <punpunonodera754@gmail.com>
2026-08-08 16:12:09 +0600

    feat: Add Tweet Class

---
+++ TwitterClone/TwitterClone.Domain/Entities/Tweet.cs
@@ -0,0 +1,36 @@
+﻿namespace TwitterClone.Domain.Entities
+{
+    public class Tweet
+    {
+        public Guid Id { get; private set; }
+        public Guid AuthorId { get; private set; }
+        public string Content { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime? UpdatedAt { get; private set; }
+
+        public Tweet(Guid authorId, string content)
+        {
+            Id = Guid.NewGuid();
+            AuthorId = authorId;
+            CreatedAt = DateTime.UtcNow;
+            SetContent(content);
+        }
+
+        public void Edit(string newContent)
+        {
+            SetContent(newContent);
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        private void SetContent(string content)
+        {
+            if (string.IsNullOrWhiteSpace(content))
+                throw new ArgumentException("Tweet content cannot be empty.");
+
+            if (content.Length > 280)
+                throw new ArgumentException("Tweet cannot exceed 280 characters.");
+
+            Content = content;
+        }
+    }
+}
