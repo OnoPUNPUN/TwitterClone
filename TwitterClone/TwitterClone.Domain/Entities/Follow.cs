85726f8d5977c9fe63d3d1da436f9c11b21186a3
OnoPUNPUN <punpunonodera754@gmail.com>
2026-08-08 16:13:51 +0600

    feat: Add Follow Class

---
+++ TwitterClone/TwitterClone.Domain/Entities/Follow.cs
@@ -0,0 +1,19 @@
+﻿namespace TwitterClone.Domain.Entities
+{
+    public class Follow
+    {
+        public Guid FollowerId { get; private set; } // The user who is following
+        public Guid FolloweeId { get; private set; } // The user being followed
+        public DateTime FollowedAt { get; private set; }
+
+        public Follow(Guid followerId, Guid followeeId)
+        {
+            if (followerId == followeeId)
+                throw new ArgumentException("A user cannot follow themselves.");
+
+            FollowerId = followerId;
+            FolloweeId = followeeId;
+            FollowedAt = DateTime.UtcNow;
+        }
+    }
+}
