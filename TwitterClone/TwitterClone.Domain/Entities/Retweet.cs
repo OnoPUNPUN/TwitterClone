f3cd83ac74e4aeae93cb750553f67bdc3ceb4dfe
OnoPUNPUN <punpunonodera754@gmail.com>
2026-08-08 16:13:29 +0600

    feat: Add Retweet Class

---
+++ TwitterClone/TwitterClone.Domain/Entities/Retweet.cs
@@ -0,0 +1,16 @@
+﻿namespace TwitterClone.Domain.Entities
+{
+    public class Retweet
+    {
+        public Guid UserId { get; private set; }
+        public Guid TweetId { get; private set; }
+        public DateTime RetweetedAt { get; private set; }
+
+        public Retweet(Guid userId, Guid tweetId)
+        {
+            UserId = userId;
+            TweetId = tweetId;
+            RetweetedAt = DateTime.UtcNow;
+        }
+    }
+}
